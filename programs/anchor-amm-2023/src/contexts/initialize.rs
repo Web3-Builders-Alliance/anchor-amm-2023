34e5e1a8dc33e95e9097706266665f431b3f11be
Richard <ivmidable@gmail.com>
2023-11-19 11:17:31 -0500

    add anchor 0.29.0

--- programs/anchor-amm-2023/src/contexts/initialize.rs
+++ programs/anchor-amm-2023/src/contexts/initialize.rs
@@ -1,10 +1,8 @@
-use std::collections::BTreeMap;
-
-use anchor_lang::{prelude::*};
-use anchor_spl::token::{Mint, Token, TokenAccount};
-use anchor_spl::associated_token::AssociatedToken;
 use crate::errors::AmmError;
 use crate::state::config::Config;
+use anchor_lang::prelude::*;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 #[derive(Accounts)]
 #[instruction(seed: u64)]
@@ -40,43 +38,37 @@ pub struct Initialize<'info> {
     #[account(seeds = [b"auth"], bump)]
     pub auth: UncheckedAccount<'info>,
     #[account(
-        init, 
-        payer = initializer, 
-        seeds = [b"config", seed.to_le_bytes().as_ref()], 
+        init,
+        payer = initializer,
+        seeds = [b"config", seed.to_le_bytes().as_ref()],
         bump,
         space = Config::LEN
     )]
     pub config: Account<'info, Config>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
 impl<'info> Initialize<'info> {
     pub fn init(
         &mut self,
-        bumps: &BTreeMap<String, u8>,
+        bumps: &InitializeBumps,
         seed: u64,
         fee: u16,
-        authority: Option<Pubkey>        
+        authority: Option<Pubkey>,
     ) -> Result<()> {
         // Don't charge >100.00% as a fee
         require!(fee <= 10000, AmmError::InvalidFee);
-
-        let (auth_bump, config_bump, lp_bump) = (
-            *bumps.get("auth").ok_or(AmmError::BumpError)?,
-            *bumps.get("config").ok_or(AmmError::BumpError)?,
-            *bumps.get("mint_lp").ok_or(AmmError::BumpError)?
-        );
         self.config.init(
             seed,
             authority,
             self.mint_x.key(),
             self.mint_y.key(),
             fee,
-            auth_bump,
-            config_bump,
-            lp_bump
+            bumps.auth,
+            bumps.config,
+            bumps.mint_lp,
         );
         Ok(())
     }
